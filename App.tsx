893122ae5ba03d6a8711f809d7e07d1b045e0f80
MintakanValerie <67020516+MintakanValerie@users.noreply.github.com>
2021-02-23 22:07:55 -0800

    Sync authenticated user to DB

--- App.tsx
+++ App.tsx
@@ -1,19 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { withAuthenticator } from 'aws-amplify-react-native'
-import Amplify from 'aws-amplify'
+import Amplify, {
+  Auth,
+  API,
+  graphqlOperation
+} from 'aws-amplify'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import config from './aws-exports'
+import config from './aws-exports';
+import { getUser } from "./Users/miaoyuli/TwitterClone/graphql/queries";
+import { createUser } from "./Users/miaoyuli/TwitterClone/graphql/mutations";
 
 Amplify.configure(config)
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return 'https://pbs.twimg.com/media/EubI9dSUUAIS6JJ?format=jpg&name=4096x4096';
+  }
+
+  const saveUserToDB = async (user) => {
+    console.log(user);
+    await API.graphql(graphqlOperation(createUser, {input: user}))
+  }
+
+  useEffect(() => {
+    const updateUser = async () => {
+      // Get current authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+
+      if(userInfo) {
+        // Check if user already exists in database
+        const userData = await API.graphql(graphqlOperation(getUser, {id: userInfo.attributes.sub}));
+        console.log(userData)
+        if(!userData.data.getUser) {
+          const user = {
+            id: userInfo.attributes.sub,
+            username: userInfo.username,
+            name: userInfo.username,
+            email: userInfo.attributes.email,
+            image: getRandomImage()
+          }
+          await saveUserToDB(user);
+        } else {
+          console.log('User already exists.')
+        }
+      }
+
+      // If it doesn't, create the user in the database
+    }
+    updateUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
