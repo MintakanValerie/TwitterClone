c47bbb766dcc72af11bd4999bbfe784d678a7047
MintakanValerie <67020516+MintakanValerie@users.noreply.github.com>
2021-02-27 06:09:02 -0800

    Connnect the Feed with Tweets from backend

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, FlatList } from "react-native";
 import tweets from "../../data/tweet";
 import Tweet from "../Tweet";
+import {API, graphqlOperation} from "aws-amplify";
+import {listTweets} from "../../Users/miaoyuli/TwitterClone/graphql/queries";
 
-const Feed = () => (
-  <View style={{ flex: 1, width: '100%' }}>
-    <FlatList
+const Feed = () => {
+
+  const [tweets, setTweet] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchTweets = async () => {
+    setLoading(true);
+    // get tweets from backend, and set them to state
+    try {
+      const tweetData = await API.graphql(graphqlOperation(listTweets));
+      setTweet(tweetData.data.listTweets.items);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchTweets();
+  }, [])
+
+  return (
+    <View style={{ flex: 1, width: '100%' }}>
+      <FlatList
         data={tweets}
         renderItem={({item}) => <Tweet tweet={item} />}
         keyExtractor={(item) => item.id}
-    />
-  </View>
-)
+        refreshing={loading}
+        onRefresh={fetchTweets}
+      />
+    </View>
+  );
+
+};
 
 export default Feed;
